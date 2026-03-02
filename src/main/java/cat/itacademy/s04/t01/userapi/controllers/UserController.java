a10a2ec2282ccc796c54f3ad21c2eec78c23d6e9
federicopratico <federico.pratico@icloud.com>
2026-03-02 18:29:36 +0100

    refactored(UserController): change add method logic - read request body with dto following the standard - return the created object

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,11 +1,15 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.dtos.UserDTO;
 import cat.itacademy.s04.t01.userapi.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -14,6 +18,18 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAll() {
         return userDatabase;
+    }
+
+    @PostMapping("/users")
+    public User add(@RequestBody UserDTO userDTO) {
+        UUID newUserId = UUID.randomUUID();
+        String newUserName = userDTO.name();
+        String newUserEmail = userDTO.email();
+
+        User newUser = new User(newUserId, newUserName, newUserEmail);
+        userDatabase.add(newUser);
+
+        return newUser;
     }
 
 }
