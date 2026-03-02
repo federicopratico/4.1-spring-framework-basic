6cc542afe5879d676d94e845011822efc82785a3
federicopratico <federico.pratico@icloud.com>
2026-03-02 17:06:19 +0100

    feature(User): change User to Record type

--- src/main/java/cat/itacademy/s04/t01/userapi/model/User.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/model/User.java
@@ -2,10 +2,8 @@ package cat.itacademy.s04.t01.userapi.model;
 
 import java.util.UUID;
 
-public class User {
-    private UUID id;
-    private String name;
-    private String email;
-
-
+public record User(
+        UUID id,
+        String name,
+        String email) {
 }
