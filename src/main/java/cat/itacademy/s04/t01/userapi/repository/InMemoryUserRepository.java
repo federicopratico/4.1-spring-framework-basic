eeef007e7ae4855e16b7ebc303b3121883daee59
federicopratico <federico.pratico@icloud.com>
2026-03-03 23:27:18 +0100

    fix(InMemoryUserRepository): fixed implementation's logic errors

--- src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
@@ -1,13 +1,14 @@
 package cat.itacademy.s04.t01.userapi.repository;
 
-import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
 import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public class InMemoryUserRepository implements UserRepository {
 
     private static final List<User> userDatabase = new ArrayList<>();
@@ -15,8 +16,7 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public User save(User user) {
         userDatabase.add(user);
-        int index = userDatabase.indexOf(user);
-        return userDatabase.get(index);
+        return user;
     }
 
     @Override
@@ -26,10 +26,9 @@ public class InMemoryUserRepository implements UserRepository {
 
     @Override
     public Optional<User> findById(UUID id) {
-        return Optional.ofNullable(userDatabase.stream()
+        return userDatabase.stream()
                 .filter(user -> user.id().equals(id))
-                .findFirst()
-                .orElseThrow(UserNotFoundException::new));
+                .findFirst();
     }
 
     @Override
@@ -43,5 +42,10 @@ public class InMemoryUserRepository implements UserRepository {
     public boolean existsByEmail(String email) {
         return userDatabase.stream()
                 .anyMatch(user -> user.email().equalsIgnoreCase(email));
+    }
+
+    // test purpose method
+    public void clear() {
+        userDatabase.clear();
     }
 }
