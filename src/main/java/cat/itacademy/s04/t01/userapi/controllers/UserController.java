d4a091dfec00502e15412d2894468cdec4ef2b31
federicopratico <federico.pratico@icloud.com>
2026-03-04 20:15:07 +0100

    refactor: change add method logic, now it's managing no buisness logic

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,54 +1,41 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.dtos.UserDTO;
-import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
 import cat.itacademy.s04.t01.userapi.model.User;
+import cat.itacademy.s04.t01.userapi.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 public class UserController {
-    private static final List<User> userDatabase = new ArrayList<>();
 
-    @GetMapping("/users")
-    public List<User> getAll(@RequestParam(required = false) String name) {
-        if(name != null && !name.isBlank()) {
-            return userDatabase.stream()
-                    .filter(user -> user.name().toLowerCase().contains(name.toLowerCase()))
-                    .toList();
-        }
+    private final UserService service;
 
-        return userDatabase;
-    }
-
-    @GetMapping("/users/{id}")
-    public User getByID(@PathVariable UUID id) {
-        return userDatabase.stream()
-                .filter(user -> user.id().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new UserNotFoundException("user not found"));
+    public UserController(UserService service) {
+        this.service = service;
     }
 
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.CREATED)
     public User add(@RequestBody UserDTO userDTO) {
-        UUID newUserId = UUID.randomUUID();
-        String newUserName = userDTO.name();
-        String newUserEmail = userDTO.email();
-
-        User newUser = new User(newUserId, newUserName, newUserEmail);
-        userDatabase.add(newUser);
-
-        return newUser;
+        return service.createUser(userDTO);
     }
 
-    // used for test execution
-    public void clearDatabase() {
-        userDatabase.clear();
+    @GetMapping("/users")
+    public List<User> getAll(@RequestParam(required = false) String name) {
+        if(name != null && !name.isBlank()) {
+            return service.searchUsersByName(name);
+        }
+
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getByID(@PathVariable UUID id) {
+        return service.getUserById(id);
     }
 
 }
