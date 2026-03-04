92ad0ec9720767aebe0e4962f3cac194735fed0b
federicopratico <federico.pratico@icloud.com>
2026-03-04 20:12:57 +0100

    refactor: change createUser method signature to accept UserDTO

--- src/main/java/cat/itacademy/s04/t01/userapi/service/UserService.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/service/UserService.java
@@ -1,12 +1,13 @@
 package cat.itacademy.s04.t01.userapi.service;
 
+import cat.itacademy.s04.t01.userapi.dtos.UserDTO;
 import cat.itacademy.s04.t01.userapi.model.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-    User createUser(User user);
+    User createUser(UserDTO user);
     List<User> getAllUsers();
     User getUserById(UUID id);
     List<User> searchUsersByName(String name);
