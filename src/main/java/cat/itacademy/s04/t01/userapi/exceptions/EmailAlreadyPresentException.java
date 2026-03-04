c2135e59cbd5472e32d8c13de47327f4dbf4d0bb
federicopratico <federico.pratico@icloud.com>
2026-03-04 17:42:27 +0100

    refactor(EmailAlreadyPresentException): added spring annotation to the return http status

--- src/main/java/cat/itacademy/s04/t01/userapi/exceptions/EmailAlreadyPresentException.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/exceptions/EmailAlreadyPresentException.java
@@ -1,5 +1,9 @@
 package cat.itacademy.s04.t01.userapi.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class EmailAlreadyPresentException extends RuntimeException {
     static private final String MESSAGE = "the email already exists";
     public EmailAlreadyPresentException() {
